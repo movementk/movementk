f5d198637b14954c603e2e6de7b0186d3d01914f
kimyh5392 <mk.kimyh@gmail.com>
2018-05-21 18:08:18 +0900

    ie 꺠짐 충돌방지 추가

--- inc/dochead.php
+++ inc/dochead.php
@@ -4,7 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <title>무브먼트케이그룹</title>
         <link rel="stylesheet" href="/assets/fontello/css/fontello.css">
         <link rel="stylesheet" href="/assets/css/common.css">
         <link rel="stylesheet" href="/assets/css/default.css">
-        <title>무브먼트케이그룹</title>
+        <!--[if IE 9]>
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie9.min.css" rel="stylesheet">
+        <![endif]-->
+        <!--[if lte IE 8]>
+            <link href="https://cdn.jsdelivr.net/gh/coliff/bootstrap-ie8/css/bootstrap-ie8.min.css" rel="stylesheet">
+            <script src="https://cdn.jsdelivr.net/g/html5shiv@3.7.3"></script>
+        <![endif]-->
